61b1a6505e209ff1444490af5c8f7101f921bd86
mohitpanwar09 <sandypanwar09@gmail.com>
2020-08-23 16:57:52 +0530

    added splash screen

---
+++ app/src/main/java/com/example/letstalk/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.letstalk
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler= Handler()
+        handler.postDelayed({
+
+            val intent=Intent(this,MainActivity::class.java)
+            intent.flags=Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+
+        },4000)
+    }
+}
